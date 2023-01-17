d685165bed70499e241f7de4b42d3ab457c3412f
Artyom Liou <artyomliou@gmail.com>
2023-01-17 13:25:16 +0800

    style(decoder): replace if-else with switch

--- src/decoder/decoder.ts
+++ src/decoder/decoder.ts
@@ -58,166 +58,236 @@ function parseBuffer(
   view: DataView,
   builder: StructBuilder
 ) {
+  // Possiblly used variables
+  let sizeByte: number
+  let dataByte: number
+  let size: number
+  let bigint: bigint
+
+  // Iteration
   let pos = 0
-  while (pos < view.byteLength) {
+  READ_NEXT: while (pos < view.byteLength) {
     // Get first byte & move pointer for resolving value
     const firstByte = view.getUint8(pos)
     pos++
 
+    // String, array, map, uint, int
     if (firstByte >= 0xa0 && firstByte <= 0xbf) {
-      const sizeByte = 0
-      const dataByte = firstByte - 0xa0
+      dataByte = firstByte - 0xa0
       builder.insertValue(
-        decodeStrWithFlexibleSize(buffer, pos, sizeByte, dataByte)
+        decodeStrWithFlexibleSize(buffer.subarray(pos + 0, pos + 0 + dataByte))
       )
-      pos += sizeByte + dataByte
-    } else if (firstByte === STR8_PREFIX) {
-      const sizeByte = 1
-      const dataByte = view.getUint8(pos)
-      builder.insertValue(
-        decodeStrWithFlexibleSize(buffer, pos, sizeByte, dataByte)
-      )
-      pos += sizeByte + dataByte
-    } else if (firstByte === STR16_PREFIX) {
-      const sizeByte = 2
-      const dataByte = view.getUint16(pos, false)
-      builder.insertValue(
-        decodeStrWithFlexibleSize(buffer, pos, sizeByte, dataByte)
-      )
-      pos += sizeByte + dataByte
-    } else if (firstByte === STR32_PREFIX) {
-      const sizeByte = 4
-      const dataByte = view.getUint32(pos, false)
-      builder.insertValue(
-        decodeStrWithFlexibleSize(buffer, pos, sizeByte, dataByte)
-      )
-      pos += sizeByte + dataByte
+      pos += 0 + dataByte
+      continue READ_NEXT
+    } else if (firstByte >= 0x90 && firstByte <= 0x9f) {
+      size = firstByte - 0b10010000
+      builder.newStruct(new Array(size), size)
+      continue READ_NEXT
+    } else if (firstByte >= 0x80 && firstByte <= 0x8f) {
+      builder.newStruct({}, firstByte - 0x80)
+      continue READ_NEXT
     } else if (firstByte >= 0x00 && firstByte <= 0x7f) {
       builder.insertValue(firstByte)
+      continue READ_NEXT
     } else if (firstByte >= 0xe0 && firstByte <= 0xff) {
       builder.insertValue(view.getInt8(pos - 1))
-    } else if (firstByte >= 0x90 && firstByte <= 0x9f) {
-      const size = firstByte - 0b10010000
-      builder.newStruct(new Array(size), size)
-    } else if (firstByte === ARRAY16_PREFIX) {
-      const size = view.getUint16(pos, false)
-      builder.newStruct(new Array(size), size)
-      pos += 2
-    } else if (firstByte === ARRAY32_PREFIX) {
-      const size = view.getUint32(pos, false)
-      builder.newStruct(new Array(size), size)
-      pos += 4
-    } else if (firstByte >= 0x80 && firstByte <= 0x8f) {
-      const size = firstByte - 0x80
-      builder.newStruct({}, size)
-    } else if (firstByte === MAP16_PREFIX) {
-      const size = view.getUint16(pos, false)
-      builder.newStruct({}, size)
-      pos += 2
-    } else if (firstByte === MAP32_PREFIX) {
-      const size = view.getUint32(pos, false)
-      builder.newStruct({}, size)
-      pos += 4
-    } else if (firstByte === UINT8_PREFIX) {
-      builder.insertValue(view.getUint8(pos))
-      pos += 1
-    } else if (firstByte === UINT16_PREFIX) {
-      builder.insertValue(view.getUint16(pos, false))
-      pos += 2
-    } else if (firstByte === UINT32_PREFIX) {
-      builder.insertValue(view.getUint32(pos, false))
-      pos += 4
-    } else if (firstByte === UINT64_PREFIX) {
-      const bigint = view.getBigUint64(pos, false)
-      builder.insertValue(bigint >> 53n > 0 ? bigint : Number(bigint))
-      pos += 8
-    } else if (firstByte === INT8_PREFIX) {
-      builder.insertValue(view.getInt8(pos))
-      pos += 1
-    } else if (firstByte === INT16_PREFIX) {
-      builder.insertValue(view.getInt16(pos, false))
-      pos += 2
-    } else if (firstByte === INT32_PREFIX) {
-      builder.insertValue(view.getInt32(pos, false))
-      pos += 4
-    } else if (firstByte === INT64_PREFIX) {
-      const bigint = view.getBigInt64(pos, false)
-      builder.insertValue(-bigint >> 53n > 0 ? bigint : Number(bigint))
-      pos += 8
-    } else if (firstByte === FLOAT32_PREFIX) {
-      builder.insertValue(view.getFloat32(pos, false))
-      pos += 4
-    } else if (firstByte === FLOAT64_PREFIX) {
-      builder.insertValue(view.getFloat64(pos, false))
-      pos += 8
-    } else if (firstByte === NIL) {
-      builder.insertValue(null)
-    } else if (firstByte === BOOL_FALSE) {
-      builder.insertValue(false)
-    } else if (firstByte === BOOL_TRUE) {
-      builder.insertValue(true)
-    } else if (firstByte === BIN8_PREFIX) {
-      const sizeByte = 1
-      const dataByte = view.getUint8(pos)
-      builder.insertValue(
-        decodeBinWithFlexibleSize(buffer, pos, sizeByte, dataByte)
-      )
-      pos += sizeByte + dataByte
-    } else if (firstByte === BIN16_PREFIX) {
-      const sizeByte = 2
-      const dataByte = view.getUint16(pos, false)
-      builder.insertValue(
-        decodeBinWithFlexibleSize(buffer, pos, sizeByte, dataByte)
-      )
-      pos += sizeByte + dataByte
-    } else if (firstByte === BIN32_PREFIX) {
-      const sizeByte = 4
-      const dataByte = view.getUint32(pos, false)
-      builder.insertValue(
-        decodeBinWithFlexibleSize(buffer, pos, sizeByte, dataByte)
-      )
-      pos += sizeByte + dataByte
-    } else if (firstByte === FIXEXT1_PREFIX) {
-      builder.insertValue(decodeExtWithFlexibleSize(view, buffer, pos, 0, 1))
-      pos += 0 + 1 + 1
-    } else if (firstByte === FIXEXT2_PREFIX) {
-      builder.insertValue(decodeExtWithFlexibleSize(view, buffer, pos, 0, 2))
-      pos += 0 + 1 + 2
-    } else if (firstByte === FIXEXT4_PREFIX) {
-      builder.insertValue(decodeExtWithFlexibleSize(view, buffer, pos, 0, 4))
-      pos += 0 + 1 + 2
-    } else if (firstByte === FIXEXT8_PREFIX) {
-      builder.insertValue(decodeExtWithFlexibleSize(view, buffer, pos, 0, 8))
-      pos += 0 + 1 + 8
-    } else if (firstByte === FIXEXT16_PREFIX) {
-      builder.insertValue(decodeExtWithFlexibleSize(view, buffer, pos, 0, 16))
-      pos += 0 + 1 + 16
-    } else if (firstByte === EXT8_PREFIX) {
-      const sizeByte = 1
-      const dataByte = view.getUint8(pos)
-      builder.insertValue(
-        decodeExtWithFlexibleSize(view, buffer, pos, sizeByte, dataByte)
-      )
-      pos += sizeByte + 1 + dataByte
-    } else if (firstByte === EXT16_PREFIX) {
-      const sizeByte = 2
-      const dataByte = view.getUint16(pos, false)
-      builder.insertValue(
-        decodeExtWithFlexibleSize(view, buffer, pos, sizeByte, dataByte)
-      )
-      pos += sizeByte + 1 + dataByte
-    } else if (firstByte === EXT32_PREFIX) {
-      const sizeByte = 4
-      const dataByte = view.getUint32(pos, false)
-      builder.insertValue(
-        decodeExtWithFlexibleSize(view, buffer, pos, sizeByte, dataByte)
-      )
-      pos += sizeByte + 1 + dataByte
-    } else {
-      const firtByteHex = firstByte.toString(16)
-      console.error("Unknown first byte.", firtByteHex)
-      throw new Error(`Unknown first byte. (${firtByteHex})`)
+      continue READ_NEXT
     }
+
+    // String, array, map, uint, int, float, null, bool, bin, ext
+    switch (firstByte) {
+      case STR8_PREFIX:
+        dataByte = view.getUint8(pos)
+        builder.insertValue(
+          decodeStrWithFlexibleSize(
+            buffer.subarray(pos + 1, pos + 1 + dataByte)
+          )
+        )
+        pos += 1 + dataByte
+        continue READ_NEXT
+
+      case STR16_PREFIX:
+        dataByte = view.getUint16(pos, false)
+        builder.insertValue(
+          decodeStrWithFlexibleSize(
+            buffer.subarray(pos + 2, pos + 2 + dataByte)
+          )
+        )
+        pos += 2 + dataByte
+        continue READ_NEXT
+
+      case STR32_PREFIX:
+        dataByte = view.getUint32(pos, false)
+        builder.insertValue(
+          decodeStrWithFlexibleSize(
+            buffer.subarray(pos + 4, pos + 4 + dataByte)
+          )
+        )
+        pos += 4 + dataByte
+        continue READ_NEXT
+
+      case ARRAY16_PREFIX:
+        size = view.getUint16(pos, false)
+        builder.newStruct(new Array(size), size)
+        pos += 2
+        continue READ_NEXT
+
+      case ARRAY32_PREFIX:
+        size = view.getUint32(pos, false)
+        builder.newStruct(new Array(size), size)
+        pos += 4
+        continue READ_NEXT
+
+      case MAP16_PREFIX:
+        size = view.getUint16(pos, false)
+        builder.newStruct({}, size)
+        pos += 2
+        continue READ_NEXT
+
+      case MAP32_PREFIX:
+        size = view.getUint32(pos, false)
+        builder.newStruct({}, size)
+        pos += 4
+        continue READ_NEXT
+
+      case UINT8_PREFIX:
+        builder.insertValue(view.getUint8(pos))
+        pos += 1
+        continue READ_NEXT
+
+      case UINT16_PREFIX:
+        builder.insertValue(view.getUint16(pos, false))
+        pos += 2
+        continue READ_NEXT
+
+      case UINT32_PREFIX:
+        builder.insertValue(view.getUint32(pos, false))
+        pos += 4
+        continue READ_NEXT
+
+      case UINT64_PREFIX:
+        bigint = view.getBigUint64(pos, false)
+        builder.insertValue(bigint >> 53n > 0 ? bigint : Number(bigint))
+        pos += 8
+        continue READ_NEXT
+
+      case INT8_PREFIX:
+        builder.insertValue(view.getInt8(pos))
+        pos += 1
+        continue READ_NEXT
+
+      case INT16_PREFIX:
+        builder.insertValue(view.getInt16(pos, false))
+        pos += 2
+        continue READ_NEXT
+
+      case INT32_PREFIX:
+        builder.insertValue(view.getInt32(pos, false))
+        pos += 4
+        continue READ_NEXT
+
+      case INT64_PREFIX:
+        bigint = view.getBigInt64(pos, false)
+        builder.insertValue(-bigint >> 53n > 0 ? bigint : Number(bigint))
+        pos += 8
+        continue READ_NEXT
+
+      case FLOAT32_PREFIX:
+        builder.insertValue(view.getFloat32(pos, false))
+        pos += 4
+        continue READ_NEXT
+
+      case FLOAT64_PREFIX:
+        builder.insertValue(view.getFloat64(pos, false))
+        pos += 8
+        continue READ_NEXT
+
+      case NIL:
+        builder.insertValue(null)
+        continue READ_NEXT
+
+      case BOOL_FALSE:
+        builder.insertValue(false)
+        continue READ_NEXT
+
+      case BOOL_TRUE:
+        builder.insertValue(true)
+        continue READ_NEXT
+
+      case BIN8_PREFIX:
+        dataByte = view.getUint8(pos)
+        builder.insertValue(buffer.subarray(pos + 1, pos + 1 + dataByte))
+        pos += 1 + dataByte
+        continue READ_NEXT
+
+      case BIN16_PREFIX:
+        dataByte = view.getUint16(pos, false)
+        builder.insertValue(buffer.subarray(pos + 2, pos + 2 + dataByte))
+        pos += 2 + dataByte
+        continue READ_NEXT
+
+      case BIN32_PREFIX:
+        dataByte = view.getUint32(pos, false)
+        builder.insertValue(buffer.subarray(pos + 4, pos + 4 + dataByte))
+        pos += 4 + dataByte
+        continue READ_NEXT
+
+      case FIXEXT1_PREFIX:
+        builder.insertValue(decodeExtWithFlexibleSize(buffer, view, pos, 0, 1))
+        pos += 0 + 1 + 1
+        continue READ_NEXT
+
+      case FIXEXT2_PREFIX:
+        builder.insertValue(decodeExtWithFlexibleSize(buffer, view, pos, 0, 2))
+        pos += 0 + 1 + 2
+        continue READ_NEXT
+
+      case FIXEXT4_PREFIX:
+        builder.insertValue(decodeExtWithFlexibleSize(buffer, view, pos, 0, 4))
+        pos += 0 + 1 + 4
+        continue READ_NEXT
+
+      case FIXEXT8_PREFIX:
+        builder.insertValue(decodeExtWithFlexibleSize(buffer, view, pos, 0, 8))
+        pos += 0 + 1 + 8
+        continue READ_NEXT
+
+      case FIXEXT16_PREFIX:
+        builder.insertValue(decodeExtWithFlexibleSize(buffer, view, pos, 0, 16))
+        pos += 0 + 1 + 16
+        continue READ_NEXT
+
+      case EXT8_PREFIX:
+        sizeByte = 1
+        dataByte = view.getUint8(pos)
+        builder.insertValue(
+          decodeExtWithFlexibleSize(buffer, view, pos, sizeByte, dataByte)
+        )
+        pos += sizeByte + 1 + dataByte
+        continue READ_NEXT
+
+      case EXT16_PREFIX:
+        sizeByte = 2
+        dataByte = view.getUint16(pos, false)
+        builder.insertValue(
+          decodeExtWithFlexibleSize(buffer, view, pos, sizeByte, dataByte)
+        )
+        pos += sizeByte + 1 + dataByte
+        continue READ_NEXT
+
+      case EXT32_PREFIX:
+        sizeByte = 4
+        dataByte = view.getUint32(pos, false)
+        builder.insertValue(
+          decodeExtWithFlexibleSize(buffer, view, pos, sizeByte, dataByte)
+        )
+        pos += sizeByte + 1 + dataByte
+        continue READ_NEXT
+    }
+
+    const firtByteHex = firstByte.toString(16)
+    console.error("Unknown first byte.", firtByteHex)
+    throw new Error(`Unknown first byte. (${firtByteHex})`)
   }
 }
 
@@ -249,74 +319,39 @@ export function optIn(opt: Options) {
 const trie = new PrefixTrie("Short key trie")
 const textDecoder = new TextDecoder()
 
-function decodeStrWithFlexibleSize(
-  buffer: Uint8Array,
-  pos: number,
-  sizeByteLength: number,
-  dataByteLength: number
-): string {
-  const strDataRange = calculateDataRange(pos, sizeByteLength, dataByteLength)
-  const buf = buffer.subarray(strDataRange.start, strDataRange.end)
-  if (shortStringCacheEnabled && dataByteLength < shortStringCacheLessThan) {
-    let result = trie.search(buf)
+function decodeStrWithFlexibleSize(strBuf: Uint8Array): string {
+  if (shortStringCacheEnabled && strBuf.byteLength < shortStringCacheLessThan) {
+    let result = trie.search(strBuf)
     if (result) {
       return result
     }
-    result = utf8Decode(buf)
-    trie.insert(buf, result)
+    result = utf8Decode(strBuf)
+    trie.insert(strBuf, result)
     return result
-  } else if (jsUtf8DecodeEnabled && dataByteLength < jsUtf8DecodeLessThan) {
-    return utf8Decode(buf)
+  } else if (jsUtf8DecodeEnabled && strBuf.byteLength < jsUtf8DecodeLessThan) {
+    return utf8Decode(strBuf)
   } else {
-    return textDecoder.decode(buf)
+    return textDecoder.decode(strBuf)
   }
-}
-
-// Bin extracting
-function decodeBinWithFlexibleSize(
-  uint8View: Uint8Array,
-  pos: number,
-  sizeByteLength: number,
-  dataByteLength: number
-): Uint8Array {
-  const binDataRange = calculateDataRange(pos, sizeByteLength, dataByteLength)
-  return uint8View.subarray(binDataRange.start, binDataRange.end)
 }
 
 // Extension decoding
 function decodeExtWithFlexibleSize(
+  buffer: Uint8Array,
   view: DataView,
-  uint8View: Uint8Array,
   pos: number,
-  sizeByteLength: number,
-  dataByteLength: number
+  sizeByte: number,
+  dataByte: number
 ): unknown {
-  // Reminder: At this point, pos is after firstByte
-  const extType = view.getInt8(pos + sizeByteLength)
-
-  // Offset should include "size" and "type"
-  const extDataRange = calculateDataRange(
-    pos,
-    sizeByteLength + 1,
-    dataByteLength
-  )
-  const data = uint8View.subarray(extDataRange.start, extDataRange.end)
-
-  // Postprocess for supported extType
-  const ext = getExtension(extType)
-  if (typeof ext === "undefined") {
+  const ext = getExtension(view.getInt8(pos + sizeByte))
+  if (!ext) {
     throw new Error("Does not support unknown ext type.")
   }
-  return ext.decode(data)
-}
-
-function calculateDataRange(
-  pos: number,
-  offset = 0,
-  dataByteLength = 0
-): { start: number; end: number } {
-  return {
-    start: pos + offset, // inclusive
-    end: pos + offset + dataByteLength, // exclusive
-  }
+  const typeByte = 1
+  return ext.decode(
+    buffer.subarray(
+      pos + sizeByte + typeByte,
+      pos + sizeByte + typeByte + dataByte
+    )
+  )
 }
